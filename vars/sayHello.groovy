41414a4d55614a3690613b0acfad9c8c7f70e132
wanglong <wanglong@coohua.com>
2020-07-01 11:38:32 +0800

    sss

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,30 +1,28 @@
- def call(String appname) {
-   echo "Hello, ${appname}."
+def call(String name = 'jdk',String appname,String tag,String denv,String target) {
+   echo "Hello, ${name}."
    stage('Clone'){
-   	echo "clone"
-   	sh """
-   	 python /tmp/test.py
-      /tmp/test.sh appname
-   		"""
-      }
+      echo "clone"
+      sh """
+       python /tmp/test.py
+     /tmp/test.sh appname tag denv target
+         """
 
    }
    stage('Test'){
-   	echo 'test'
+      echo 'test'
 
    }
    stage('Build'){
-   	echo 'build'
+      echo 'build'
 
    }
 
    stage('容器镜像构建'){
-   	echo 'docker build'
+      echo 'docker build'
 
    }
 
    stage('Deploy to k8s'){
-   	echo 'deploy'
+      echo 'deploy'
 
    }
- }
